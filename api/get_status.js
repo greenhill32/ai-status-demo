1fce997c85e5de41728b0d5480d924bdcb224468
greenhill32 <46974396+greenhill32@users.noreply.github.com>
2025-07-24 19:12:31 +0100

    Delete api/get_status.js

--- api/get_status.js
+++
@@ -1,8 +0,0 @@
-let currentStatus = 'Offline';
-
-export default function handler(req, res) {
-  res.status(200).json({ status: currentStatus });
-}
-
-// Export for sharing
-export const statusStore = { currentStatus };
